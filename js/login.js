e80c8ade04d409b7722e8016304b8efec1630315
Astrues <1480733667@qq.com>
2022-02-21 14:02:56 +0800

    登录

--- js/login.js
+++ js/login.js
@@ -56,7 +56,7 @@ window.addEventListener("load", () => {
     top.addEventListener("mousedown", (e) => {
         var x = e.pageX - login.offsetLeft;
         var y = e.pageY - login.offsetTop;
-        // (2) 鼠标移动的时候，把鼠标在页面中的坐标，减去 鼠标在盒子内的坐标就是模态框的left和top值
+        // (2) 鼠标移动的时候，把鼠标在页面中的坐标，减去 鼠标在盒子内的坐标就是模态框的left和top值.
         document.addEventListener('mousemove', move)
 
         function move(e) {
